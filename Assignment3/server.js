7ff492ab0c3d7e15ea26f7370a3665339904908d
AaizaAamer <aaizaaamer3@gmail.com>
2026-05-14 09:41:45 +0500

    Updated project files

--- Assignment3/server.js
+++ Assignment3/server.js
@@ -26,6 +26,8 @@ app.get("/", (req, res) => {
     res.render("index");
 });
 
-app.listen(3000, () => {
-    console.log("Server Running");
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
 });
